1e0e36237bb12093aef5a2e31504a0168714dd1d
jakub-mrow <jakub@mrowczynski.net>
2024-05-12 14:02:11 +0200

    update task model

--- src/tsd/models.py
+++ src/tsd/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 
 class Task(models.Model):
+    LOW = "Low"
+    MEDIUM = "Medium"
+    HIGH = "High"
+    PRIORITY_CHOICES = (
+        (LOW, "Low"),
+        (MEDIUM, "Medium"),
+        (MEDIUM, "High"),
+    )
     description = models.CharField(max_length=128, help_text="Task description")
-
+    priority = models.CharField(max_length=9, choices=PRIORITY_CHOICES, help_text="Task priority", default="Low", blank=True, null=True)
+    estimation = models.FloatField(help_text="Task estimation in hours", default=0.0, blank=True, null=True)
 
